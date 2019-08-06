d42c79e4dd89e27620905198d796d64027a25e09
Alex Rix <alexanderjrix@gmail.com>
2019-08-06 17:00:35 -0400

    fix the way mu_beta_m is calculated

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,9 +6,32 @@
 
 using namespace Rcpp;
 
+// rand_bernoulli
+int rand_bernoulli(double p);
+RcppExport SEXP _hdbm_rand_bernoulli(SEXP pSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< double >::type p(pSEXP);
+    rcpp_result_gen = Rcpp::wrap(rand_bernoulli(p));
+    return rcpp_result_gen;
+END_RCPP
+}
+// rand_invgamma
+double rand_invgamma(double shape, double scale);
+RcppExport SEXP _hdbm_rand_invgamma(SEXP shapeSEXP, SEXP scaleSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< double >::type shape(shapeSEXP);
+    Rcpp::traits::input_parameter< double >::type scale(scaleSEXP);
+    rcpp_result_gen = Rcpp::wrap(rand_invgamma(shape, scale));
+    return rcpp_result_gen;
+END_RCPP
+}
 // run_hdbm_mcmc
-arma::vec run_hdbm_mcmc(arma::vec Y, arma::vec A, arma::mat M, arma::mat C1, arma::mat C2, arma::vec beta_m, arma::vec alpha_a, double pi_m, double pi_a, int burnin, int nsamples);
-RcppExport SEXP _hdbm_run_hdbm_mcmc(SEXP YSEXP, SEXP ASEXP, SEXP MSEXP, SEXP C1SEXP, SEXP C2SEXP, SEXP beta_mSEXP, SEXP alpha_aSEXP, SEXP pi_mSEXP, SEXP pi_aSEXP, SEXP burninSEXP, SEXP nsamplesSEXP) {
+Rcpp::List run_hdbm_mcmc(arma::vec Y, arma::vec A, arma::mat M, arma::mat C1, arma::mat C2, arma::vec beta_m_init, arma::vec alpha_a_init, double pi_m_init, double pi_a_init, int burnin, int nsamples);
+RcppExport SEXP _hdbm_run_hdbm_mcmc(SEXP YSEXP, SEXP ASEXP, SEXP MSEXP, SEXP C1SEXP, SEXP C2SEXP, SEXP beta_m_initSEXP, SEXP alpha_a_initSEXP, SEXP pi_m_initSEXP, SEXP pi_a_initSEXP, SEXP burninSEXP, SEXP nsamplesSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -17,18 +40,20 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< arma::mat >::type M(MSEXP);
     Rcpp::traits::input_parameter< arma::mat >::type C1(C1SEXP);
     Rcpp::traits::input_parameter< arma::mat >::type C2(C2SEXP);
-    Rcpp::traits::input_parameter< arma::vec >::type beta_m(beta_mSEXP);
-    Rcpp::traits::input_parameter< arma::vec >::type alpha_a(alpha_aSEXP);
-    Rcpp::traits::input_parameter< double >::type pi_m(pi_mSEXP);
-    Rcpp::traits::input_parameter< double >::type pi_a(pi_aSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type beta_m_init(beta_m_initSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type alpha_a_init(alpha_a_initSEXP);
+    Rcpp::traits::input_parameter< double >::type pi_m_init(pi_m_initSEXP);
+    Rcpp::traits::input_parameter< double >::type pi_a_init(pi_a_initSEXP);
     Rcpp::traits::input_parameter< int >::type burnin(burninSEXP);
     Rcpp::traits::input_parameter< int >::type nsamples(nsamplesSEXP);
-    rcpp_result_gen = Rcpp::wrap(run_hdbm_mcmc(Y, A, M, C1, C2, beta_m, alpha_a, pi_m, pi_a, burnin, nsamples));
+    rcpp_result_gen = Rcpp::wrap(run_hdbm_mcmc(Y, A, M, C1, C2, beta_m_init, alpha_a_init, pi_m_init, pi_a_init, burnin, nsamples));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_hdbm_rand_bernoulli", (DL_FUNC) &_hdbm_rand_bernoulli, 1},
+    {"_hdbm_rand_invgamma", (DL_FUNC) &_hdbm_rand_invgamma, 2},
     {"_hdbm_run_hdbm_mcmc", (DL_FUNC) &_hdbm_run_hdbm_mcmc, 11},
     {NULL, NULL, 0}
 };
